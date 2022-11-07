d365a0a9f4b9280d7536f9116fb6614ddffe105a
mr_maroga <45645052+AdvocateM@users.noreply.github.com>
2022-11-07 16:40:49 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,12 +4,12 @@ import Search from './components/Search';
 import ShortList from './components/ShortList';
 
 function App({ data }) {
-  Constructor(props){
-    super(props)
-    this.this.state = {
-      filterText: ''
-    }
-  }
+  //Constructor(props){
+    //super(props)
+    //this.this.state = {
+     // filterText: ''
+    //}
+ // }
   const nameList = data.map(name => {
     return <li key={name.id} className={name.sex}>{name.name}</li>
   })
